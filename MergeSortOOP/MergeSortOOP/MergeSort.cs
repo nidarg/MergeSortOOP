a67db9df10b52ccb3fc8536db9e26485e6701df2
Gradin <gradin_g@yahoo.com>
2019-05-13 15:11:45 +0300

    Sort method of MergeSort class pass

--- MergeSortOOP/MergeSortOOP/MergeSort.cs
+++ MergeSortOOP/MergeSortOOP/MergeSort.cs
@@ -8,7 +8,19 @@ namespace MergeSortOOP
     {
         public int[] Sort(int[] arrToSort)
         {
-            return new[]{ 1,2,3,4};
+            if (arrToSort.Length == 1)
+                return arrToSort;
+
+            var splitter = new Splitter(arrToSort);
+            int[] firstSplittedArray = splitter.GetFirstPart();
+            int[] secondSplittedArray = splitter.GetSecondPart();
+
+            int[] firstSortedArray = Sort(firstSplittedArray);
+            int[] secondSortedArray = Sort(secondSplittedArray);
+            var merger = new Merger();
+            return merger.Merge(firstSortedArray, secondSortedArray);
+
+            
         }
     }
 }
